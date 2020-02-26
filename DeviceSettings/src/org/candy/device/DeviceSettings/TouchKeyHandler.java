AOSiP-Devices/device_oneplus_sm8150-common

sm8150-common: DeviceSettings: Add ambient as a gesture action
Change-Id: I10661bf07b2c3c1037beed4ac5ca852b5d6f0522
Signed-off-by: Anirudh Gupta <anirudhgupta109@aosip.dev>

DeviceSettings/src/com/aosip/device/DeviceSettings/TouchKeyHandler.java
@@ -57,6 +57,7 @@ public class TouchKeyHandler implements DeviceKeyHandler {
     private static final String TAG = TouchKeyHandler.class.getSimpleName();
 
     private static final String GESTURE_WAKEUP_REASON = "touchscreen-gesture-wakeup";
+    private static final String DOZE_INTENT = "com.android.systemui.doze.pulse";
     private static final int GESTURE_REQUEST = 0;
     private static final int GESTURE_WAKELOCK_DURATION = 3000;
     private static final int EVENT_PROCESS_WAKELOCK_DURATION = 500;
@@ -228,6 +229,9 @@ public class TouchKeyHandler implements DeviceKeyHandler {
                 case Constants.ACTION_VOLUME_UP:
                     volumeUp();
                     break;
+                case Constants.ACTION_AMBIENT_DISPLAY:
+                    launchDozePulse();
+                    break;
             }
         }
     }
@@ -314,6 +318,17 @@ public class TouchKeyHandler implements DeviceKeyHandler {
         mGestureWakeLock.acquire(GESTURE_WAKELOCK_DURATION);
         mAudioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_RAISE, 0);
         doHapticFeedback();
+    }
+
+    private void launchDozePulse() {
+        final boolean dozeEnabled = Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.DOZE_ENABLED, 1) != 0;
+        if (dozeEnabled) {
+            mGestureWakeLock.acquire(GESTURE_WAKELOCK_DURATION);
+            final Intent intent = new Intent(PULSE_ACTION);
+            mContext.sendBroadcastAsUser(intent, UserHandle.CURRENT);
+            doHapticFeedback();
+        }
     }
 
     private void dispatchMediaKeyWithWakeLockToMediaSession(final int keycode) {

matt0301/bootleg_oneplus_sm8150-common

DeviceSettings/src/org/candy/device/DeviceSettings/TouchKeyHandler.java
@@ -57,6 +57,7 @@ public class TouchKeyHandler implements DeviceKeyHandler {
     private static final String TAG = TouchKeyHandler.class.getSimpleName();
 
     private static final String GESTURE_WAKEUP_REASON = "touchscreen-gesture-wakeup";
+    private static final String PULSE_ACTION = "com.android.systemui.doze.pulse";
     private static final int GESTURE_REQUEST = 0;
     private static final int GESTURE_WAKELOCK_DURATION = 3000;
     private static final int EVENT_PROCESS_WAKELOCK_DURATION = 500;
@@ -228,6 +229,9 @@ public class TouchKeyHandler implements DeviceKeyHandler {
                 case Constants.ACTION_VOLUME_UP:
                     volumeUp();
                     break;
+                case Constants.ACTION_AMBIENT_DISPLAY:
+                    launchDozePulse();
+                    break;
             }
         }
     }
@@ -314,6 +318,17 @@ public class TouchKeyHandler implements DeviceKeyHandler {
         mGestureWakeLock.acquire(GESTURE_WAKELOCK_DURATION);
         mAudioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_RAISE, 0);
         doHapticFeedback();
+    }
+
+    private void launchDozePulse() {
+        final boolean dozeEnabled = Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.DOZE_ENABLED, 1) != 0;
+        if (dozeEnabled) {
+            mGestureWakeLock.acquire(GESTURE_WAKELOCK_DURATION);
+            final Intent intent = new Intent(PULSE_ACTION);
+            mContext.sendBroadcastAsUser(intent, UserHandle.CURRENT);
+            doHapticFeedback();
+        }
     }
 
     private void dispatchMediaKeyWithWakeLockToMediaSession(final int keycode) {